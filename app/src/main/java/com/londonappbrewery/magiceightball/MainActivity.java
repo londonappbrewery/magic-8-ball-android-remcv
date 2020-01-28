d01b8bb33cd63909a1b3538cbc4f82c44626336f
remcv <remuluscatana@tutanota.com>
2020-01-28 22:41:07 +0200

    Added several improvements to buttons - including disabling askButton

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,10 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
@@ -19,8 +22,10 @@ public class MainActivity extends AppCompatActivity
         final ImageView ballDisplay = findViewById(R.id.image_eightBall);
         final int[] ballArray = {R.drawable.ball1, R.drawable.ball2, R.drawable.ball3,
                             R.drawable.ball4, R.drawable.ball5};
-        Button myButton = findViewById(R.id.askButton);
+        final Button myButton = findViewById(R.id.askButton);
         Button resetButton = findViewById(R.id.resetButton);
+        final TextView mySlogan = findViewById(R.id.slogan_textView);
+        boolean stateOfSlogan = false;
 
         // button action
         myButton.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +37,8 @@ public class MainActivity extends AppCompatActivity
                 index = myRandomNumberGenerator.nextInt(5);
                 ballDisplay.setImageResource(ballArray[index]);
                 ballDisplay.setVisibility(View.VISIBLE);
+                mySlogan.setBackgroundColor(Color.parseColor("#FF5A00"));
+                myButton.setEnabled(false);
             }
         });
 
@@ -39,6 +46,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 ballDisplay.setVisibility(View.INVISIBLE);
+                mySlogan.setBackgroundColor(Color.parseColor("#F9D500"));
+                myButton.setEnabled(true);
             }
         });
 
@@ -50,6 +59,7 @@ public class MainActivity extends AppCompatActivity
 
                 index = myRandomNumberGenerator.nextInt(5);
                 ballDisplay.setImageResource(ballArray[index]);
+                mySlogan.setBackgroundColor(Color.parseColor("#F9D500"));
             }
         });
     }
