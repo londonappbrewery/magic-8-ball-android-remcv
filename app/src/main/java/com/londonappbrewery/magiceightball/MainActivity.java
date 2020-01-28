0f2746487896cc03195790e0cb325bc5c2cdc6f9
remcv <remuluscatana@tutanota.com>
2020-01-28 23:03:58 +0200

    Make askButton invisible after clicked

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity
         final Button myButton = findViewById(R.id.askButton);
         Button resetButton = findViewById(R.id.resetButton);
         final TextView mySlogan = findViewById(R.id.slogan_textView);
-        boolean stateOfSlogan = false;
 
         // button action
         myButton.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +37,7 @@ public class MainActivity extends AppCompatActivity
                 ballDisplay.setImageResource(ballArray[index]);
                 ballDisplay.setVisibility(View.VISIBLE);
                 mySlogan.setBackgroundColor(Color.parseColor("#FF5A00"));
+                myButton.setVisibility(View.INVISIBLE);
                 myButton.setEnabled(false);
             }
         });
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity
                 ballDisplay.setVisibility(View.INVISIBLE);
                 mySlogan.setBackgroundColor(Color.parseColor("#F9D500"));
                 myButton.setEnabled(true);
+                myButton.setVisibility(View.VISIBLE);
             }
         });
 
@@ -56,10 +57,21 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Random myRandomNumberGenerator = new Random();
                 int index;
-
                 index = myRandomNumberGenerator.nextInt(5);
+
+                // two possible colours: yellow #F9D500, red #FF5A00
                 ballDisplay.setImageResource(ballArray[index]);
-                mySlogan.setBackgroundColor(Color.parseColor("#F9D500"));
+
+//                if(mySlogan.getBackgroundColor().equals(Color.parseColor("#F9D500")))
+//                {
+//                    // set color to red
+//                    mySlogan.setBackgroundColor(Color.parseColor("#FF5A00"));
+//                }
+//                else if(mySlogan.getBackground().equals(Color.parseColor("#FF5A00")))
+//                {
+//                    // set color to yellow
+//                    mySlogan.setBackgroundColor(Color.parseColor("#F9D500"));
+//                }
             }
         });
     }
